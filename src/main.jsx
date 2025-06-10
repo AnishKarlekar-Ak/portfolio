8c6e1ea667144932b3dc42cf63189cf672f65ce0
AnishKarlekar-Ak <work.anishkarlekar@gmail.com>
2025-06-10 15:03:06 +0530

    2nd Commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Portfolio from './portfolio.jsx'
 import './style.css'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <SpeedInsights/>
     <Portfolio />
   </React.StrictMode>,
 )
