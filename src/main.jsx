c493bf1224e9ba118e1bb5d8570b52d6898a5eb6
AnishKarlekar-Ak <work.anishkarlekar@gmail.com>
2026-02-16 18:37:49 +0530

    Add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import Portfolio from './portfolio.jsx'
 import './style.css'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/next"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Analytics />
     <SpeedInsights/>
     <Portfolio />
   </React.StrictMode>,
